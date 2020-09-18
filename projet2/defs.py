c630dbfcdac18b96578354a6d87b3c5134c111b0
tsurubaso <tsurubaso@hotmail.com>
2020-09-19 00:48:19 +0900

    Camera Lamps project2 ok, add paths for satic lamps, path following pi, and path for Empty

--- projet2/defs.py
+++ projet2/defs.py
@@ -3,14 +3,14 @@ import random
 
 meshgr=[]
 cameragr=[]
-piotrgr=[]
-Douchkagr=[]
+emptygr=[]
+
 
 #l'ensemble des def 
 def addLamp(loc):
     bpy.ops.object.add(radius=1.0, type='LIGHT', enter_editmode=False, align='WORLD', location=loc, rotation=(0.0, 0.0, 0.0))
     bpy.context.object.data.type = 'SUN'
-    bpy.context.object.data.energy = 7
+    bpy.context.object.data.energy = 3
     
     
 def my_custom_random():
@@ -38,34 +38,12 @@ def targetobj(obj1,obj2):
     ttc.track_axis = 'TRACK_NEGATIVE_Z'
     ttc.up_axis = 'UP_Y'
     
-    #Not in use for now
-def addCamera(size, mid, pimp):
-    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(size, mid, pimp), rotation=(0, 0, 0))
-    cameraNew = bpy.context.object 
-    #to put in main.py
-    #targetobj(emptygr[0],cameraNew)
     
-def addCameraShp(size, mid, pimp):
-    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(size, mid, pimp), rotation=(0, 0, 0))
+def addCameraShp(pio, mid, pimp):
+    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(pio, mid, pimp), rotation=(0, 0, 0))
     cameraNew = bpy.context.object 
-    qsph = random.randint(0,len(meshgr)-1)
-    print(qsph)
-    targetobj(meshgr[qsph],cameraNew)
-    #targetobj(emptygr[0],cameraNew)
+    return cameraNew
     
-    #Not in use for now
-def cameraTravelingSimple(size, mid, pimp):
-    bpy.ops.curve.primitive_bezier_circle_add(radius=size, enter_editmode=False, align='WORLD', location=(0, 0, pimp))
-    path = bpy.context.object 
-    addCamera(size, mid, pimp)
-    cameraNew2=bpy.context.object
-    path.select_set(True)
-    cameraNew2.select_set(True)
-    bpy.context.view_layer.objects.active = path
-    bpy.ops.object.parent_set(type='FOLLOW') #follow paths
-    bpy.context.object.data.path_duration = 1100
-    #to put in main.py
-    #targetobj(emptygr[0],cameraNew2)
     
 def cameraTravelingSimpleShp(size, mid, pimp):
     bpy.ops.curve.primitive_bezier_circle_add(radius=size, enter_editmode=False, align='WORLD', location=(0, 0, pimp))
@@ -80,6 +58,7 @@ def cameraTravelingSimpleShp(size, mid, pimp):
     #On peut utiliser le return comme pour les mesh, ici autre methode
     bpy.ops.object.select_all(action='DESELECT')
     bpy.context.view_layer.objects.active = cameraNew2
+    return cameraNew2
     
 def putMarkers(cameragr):
     #work on Sequence Camera
